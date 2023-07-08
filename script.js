d212f030364c794c4b09adc1676c2213569002fe
Chandramohan97 <chandramsriv@gmail.com>
2023-07-09 01:18:54 +0530

    javascript : creating event handlers.

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const circleList = document.getElementsByClassName("circle");
+const circles = document.querySelector('.circle');
+// const ratings = document.getElementsByClassName('ratings');
+const footer = document.querySelector("footer");
+const primaryContainer = document.querySelector('.primaryContainer');
+const finalContainer = document.querySelector(".final-container");
+const submitBtn = document.querySelector("input");
 
+function submit(){
+    primaryContainer.style.display = "none";
+    finalContainer.style.display = "block";
+}
+
+console.log(circles);
+
+// for( i = 0 ; i < circleList.length;i++){
+//     console.log(circles[i]);
+//     // console.log(circleList[i]);
+//     // circleList[i].addEventListener('click', ()=>{
+//     //     console.log(typeof circleList[i])
+//     // })
+// 
