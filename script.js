76e2e127a6d9da96d2c0d03b6180eb66fd152d3f
Chandramohan97 <chandramsriv@gmail.com>
2023-07-09 18:26:38 +0530

    feat : code to change the color of circle to Orange if clicked upon. And changing the color of the circle which was clicked before to it's original colorl

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const circleList = document.getElementsByClassName("circle");
-const circles = document.querySelector('.circle');
-// const ratings = document.getElementsByClassName('ratings');
+const ratings = document.getElementsByClassName('ratings');
+// const circle = document.querySelectorAll(".circle");
 const footer = document.querySelector("footer");
 const primaryContainer = document.querySelector('.primaryContainer');
 const finalContainer = document.querySelector(".final-container");
@@ -11,12 +11,90 @@ function submit(){
     finalContainer.style.display = "block";
 }
 
-console.log(circles);
 
-// for( i = 0 ; i < circleList.length;i++){
-//     console.log(circles[i]);
-//     // console.log(circleList[i]);
-//     // circleList[i].addEventListener('click', ()=>{
-//     //     console.log(typeof circleList[i])
-//     // })
-// 
+
+circleList[0].addEventListener("click",()=>{
+    circleList[0].style.backgroundColor ="hsl(25, 97%, 53%)";
+
+    for( i = 0; i < circleList.length;i++){
+        if(i==0)
+            continue
+        else{
+            const circle = getComputedStyle(circleList[i])
+            const bgColor = circle.getPropertyValue("background-color");    //getPropertyValue returns value in rgb values for color,background-colro
+            if(bgColor == "rgb(251, 116, 19)"){                               // rgb value for hsl(25, 97%, 53%)
+                circleList[i].style.backgroundColor = "hsl(213, 19%, 18%)";   // changing the background color to original state.
+            }
+
+        }
+    }
+
+})
+circleList[1].addEventListener("click",()=>{
+    circleList[1].style.backgroundColor ="hsl(25, 97%, 53%)";
+    for( i = 0; i < circleList.length;i++){
+        if(i==1)
+            continue
+        else{
+            const circle = getComputedStyle(circleList[i])
+            const bgColor = circle.getPropertyValue("background-color");    //getPropertyValue returns value in rgb values for color,background-colro
+            if(bgColor == "rgb(251, 116, 19)"){                               // rgb value for hsl(25, 97%, 53%)
+                circleList[i].style.backgroundColor = "hsl(213, 19%, 18%)";   // changing the background color to original state.
+            }
+
+        }
+    }
+
+})
+
+circleList[2].addEventListener("click",()=>{
+    circleList[2].style.backgroundColor ="hsl(25, 97%, 53%)";
+
+    for( i = 0; i < circleList.length;i++){
+        if(i==2)
+            continue
+        else{
+            const circle = getComputedStyle(circleList[i])
+            const bgColor = circle.getPropertyValue("background-color");    //getPropertyValue returns value in rgb values for color,background-colro
+            if(bgColor == "rgb(251, 116, 19)"){                               // rgb value for hsl(25, 97%, 53%)
+                circleList[i].style.backgroundColor = "hsl(213, 19%, 18%)";   // changing the background color to original state.
+            }
+
+        }
+    }
+})
+
+circleList[3].addEventListener("click",()=>{
+    circleList[3].style.backgroundColor ="hsl(25, 97%, 53%)";
+
+    for( i = 0; i < circleList.length;i++){
+        if(i==3)
+            continue
+        else{
+            const circle = getComputedStyle(circleList[i])
+            const bgColor = circle.getPropertyValue("background-color");    //getPropertyValue returns value in rgb values for color,background-colro
+            if(bgColor == "rgb(251, 116, 19)"){                               // rgb value for hsl(25, 97%, 53%)
+                circleList[i].style.backgroundColor = "hsl(213, 19%, 18%)";   // changing the background color to original state.
+            }
+
+        }
+    }
+})
+
+circleList[4].addEventListener("click",()=>{
+    circleList[4].style.backgroundColor ="hsl(25, 97%, 53%)";
+
+    for( i = 0; i < circleList.length;i++){
+        if(i==4)
+            continue
+        else{
+            const circle = getComputedStyle(circleList[i])
+            const bgColor = circle.getPropertyValue("background-color");    //getPropertyValue returns value in rgb values for color,background-colro
+            if(bgColor == "rgb(251, 116, 19)"){                               // rgb value for hsl(25, 97%, 53%)
+                circleList[i].style.backgroundColor = "hsl(213, 19%, 18%)";   // changing the background color to original state.
+            }
+
+        }
+    }
+})
+
